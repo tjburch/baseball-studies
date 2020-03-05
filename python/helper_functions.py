3077c6d1f0053fa9e5da6734953122a2c77affc7
Tyler Burch <burcht11@gmail.com>
2020-03-05 10:36:18 -0600

    Add more stringers

--- python/helper_functions.py
+++ python/helper_functions.py
@@ -7,23 +7,38 @@ def scrub_stringers(df):
     ScrubSet = namedtuple('ScrubSet',"ev angle bb_type")
     to_scrub = []
 
-    # Two most prominent from each category
     # pop-ups
     to_scrub.append(ScrubSet(ev=80,angle=69,bb_type="popup"))
     to_scrub.append(ScrubSet(ev=37,angle=62,bb_type="popup"))
+    to_scrub.append(ScrubSet(ev=86,angle=67,bb_type="popup"))
+
     # Flyout
+    to_scrub.append(ScrubSet(ev=71.4,angle=36.0,bb_type="fly_ball"))
     to_scrub.append(ScrubSet(ev=89,angle=39,bb_type="fly_ball"))
-    to_scrub.append(ScrubSet(ev=89.2,angle=39.0,bb_type="fly_ball"))
+    to_scrub.append(ScrubSet(ev=89.2,angle=39.3,bb_type="fly_ball"))
+    to_scrub.append(ScrubSet(ev=97,angle=30.2,bb_type="fly_ball"))
+
     # Line Drive
-    to_scrub.append(ScrubSet(ev=91,angle=18,bb_type="line_drive"))
     to_scrub.append(ScrubSet(ev=90,angle=15,bb_type="line_drive"))
-    # Ground balls (several > 1000, scrub all)
-    to_scrub.append(ScrubSet(ev=83,angle=-21,bb_type="ground_ball"))
-    to_scrub.append(ScrubSet(ev=82.9,angle=-21.0,bb_type="ground_ball"))
-    to_scrub.append(ScrubSet(ev=41,angle=-39,bb_type="ground_ball"))
-    to_scrub.append(ScrubSet(ev=90,angle=-17,bb_type="ground_ball"))
-    to_scrub.append(ScrubSet(ev=90.3,angle=-17.3,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=90.4,angle=14.6,bb_type="line_drive"))
+    to_scrub.append(ScrubSet(ev=91,angle=18,bb_type="line_drive"))
+    to_scrub.append(ScrubSet(ev=91.1,angle=18.2,bb_type="line_drive"))
+    to_scrub.append(ScrubSet(ev=98.8,angle=17.1,bb_type="line_drive"))
+
+    # Ground balls
     to_scrub.append(ScrubSet(ev=40,angle=-36,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=40,angle=-36,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=40,angle=-36,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=40,angle=-36,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=41,angle=-39,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=43,angle=-62,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=84,angle=-20,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=83,angle=-21,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=82.9,angle=-20.7,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=90,angle=-17,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=90.2,angle=-13.0,bb_type="ground_ball"))
+    to_scrub.append(ScrubSet(ev=90.3,angle=-17.3,bb_type="ground_ball"))
+
 
     df_return = df
     for streamer in to_scrub:
